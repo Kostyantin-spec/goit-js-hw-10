eafa34dfb65f9a3f8df507aaa09fc0e97b70cfee
Kostyantin-spec <134406433+Kostyantin-spec@users.noreply.github.com>
2023-11-03 14:54:41 +0200

    ghjkk

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,17 +1,17 @@
 import axios from "axios";
 
-const API_KEY = 'live_OrfxGKcqIhq7yogqTYhfOwGe8JR7ma7hk26qWhEqi2FVLyAPpkJDGFwjNDb2ma9T '
-const BASE_URL = ' https://api.thecatapi.com/v1';
+//OrfxGKcqIhq7yogqTYhfOwGe8JR7ma7hk26qWhEqi2FVLyAPpkJDGFwjNDb2ma9T 
+const BASE_URL = 'https://api.thecatapi.com/v1';
 const END_POINT_IMAGES = 'images/search';
 const END_POINT_BREEDS = 'breeds';
-
+const API_KEY = 'live_OrfxGKcqIhq7yogqTYhfOwGe8JR7ma7hk26qWhEqi2FVLyAPpkJDGFwjNDb2ma9T ';
 
 axios.defaults.headers.common["x-api-key"] = API_KEY;
 
 export function fetchBreeds() {
-    return axion.get('${BASE_URL}/${END_POINT_BREEDS}');
+    return axios.get(`${BASE_URL}/${END_POINT_BREEDS}`);
 }
 
 export function fetchCatByBreed(breedId) {
-    return axion.get('${BASE_URL}/${END_POINT_BREEDS}?breed_ids=${breedId}');
+    return axios.get(`${BASE_URL}/${END_POINT_BREEDS}?breed_ids=${breedId}`);
 }
--- src/index.js
+++ src/index.js
@@ -1,34 +1,95 @@
 
-
-
+import { fetchBreeds } from './cat-api';
+import { fetchCatByBreed } from './cat-api';
 import SlimSelect from 'slim-select';
-
-// new SlimSelect({
-//   select: '#selectElement'
-// })
-import Notiflix from 'notiflix';
-// function fetchBreeds() {
-
-// }
-
-// function fetchCatByBreed(breedId) {
-
-// }
+import Notiflix, { Notify } from 'notiflix/build/notiflix-notify-aio.js';
 
 const refs = {
     select: document.querySelector('.js-breed-select'),
     catsInfo: document.querySelector('js-cat-info'),
     loader: document.querySelector('js-loader'),
 };
-
 const IS_HIDDEN = 'is-hidden';
 
-new SlimSelect({
+Notify.init({
     position: 'center-top',
     distance: '70px',
     timeout: '3000',
+    cssAnimationStyle: 'zoom',
     fontFamily: 'Arial, sens-serif',
   });
 
+  refs.select.classList.add(IS_HIDDEN);
+  refs.catsInfo.classList.add(IS_HIDDEN);
 
-  
+  refs.select.addEventListener('change', selectChangeHandler);
+
+  showFetchedBreeds();
+
+  function selectChangeHandler(event) {
+    const selectedCatIndex = event.currentTarget.selectedIndex;
+    const selectedId = event.currentTarget[selectedCatIndex].value;
+
+    refs.loader.classList.remove(IS_HIDEN);
+    refs.catsInfo.classList.add(IS_HIDEN);
+    refs.catsInfo.innerHTML = '';
+
+    showFetchedCatBreed(selectedId);
+  }
+  function showFetchedBreeds() {
+    fetchBreeds()
+    .then(breeds => {  
+      refs.select.insertAdjacentHTML('beforeend',
+    createMarkup(breeds.data)
+    );
+
+    new SlimSelect({
+        select: '#single',
+        settings: {
+            placeholderText: 'Select the desired cat'
+        },
+    });
+    refs.loader.classList.add(IS_HIDDEN);
+    refs.select.classList.remove(IS_HIDDEN);
+  })
+  .catch(() => {
+    refs.loader.classList.add(IS_HIDDEN);
+    Notify.warning('Failed to reguest data! Choose another breed.');
+
+  });
+  }
+ 
+
+function showFetchedCatBreed(selectedId) {
+    fetchCatByBreed(selectedId)
+    .then(cat => {
+        refs.loader.classList.add(IS_HIDDEN);
+        refs.catsInfo.classList.remove(IS_HIDDEN);
+        refs.catsInfo.innerHTML = createCatMarkup(cat.data[0]);
+    })
+.catch(() => {
+    refs.catsInfo.classList.add(IS_HIDDEN);
+    Notify.warning('Failed to request data! Choose another breed.');
+
+});
+}
+
+function createCatMarkup({ breeds, url }) {
+    const { name, description, temperament } = breeds[0];
+    return `
+      <img src="${url}" alt="${name}" height="300" class="cat-img">
+      <h2 class="cat-name">${name}</h2>
+      <p class="cat-description"><span>Description:</span> ${description}</p>
+      <p class="cat-temperament"><span>Temperament:</span> ${temperament}</p>
+  `;
+}
+ 
+ function createMarkup(breeds) {
+    return (`<option data-placeholder="true"></option>` + 
+    breeds.map(breed => createOptionMarkup(breed)).join(''));
+
+ }
+
+ function createOptionMarkup({ id, name }) {
+    return `<option value="${id}">${name}</option>`;
+ }
