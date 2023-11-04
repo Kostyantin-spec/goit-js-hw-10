58dbd47db7e3581add84befa74c323706953392d
Kostyantin-spec <134406433+Kostyantin-spec@users.noreply.github.com>
2023-11-04 20:52:30 +0200

    ghjj

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,12 +1,11 @@
-import axios from "axios";
+import axios from 'axios';
 
-//OrfxGKcqIhq7yogqTYhfOwGe8JR7ma7hk26qWhEqi2FVLyAPpkJDGFwjNDb2ma9T 
 const BASE_URL = 'https://api.thecatapi.com/v1';
 const END_POINT_BREEDS = 'breeds';
 const END_POINT_IMAGES = 'images/search';
 const API_KEY = 'live_OrfxGKcqIhq7yogqTYhfOwGe8JR7ma7hk26qWhEqi2FVLyAPpkJDGFwjNDb2ma9T';
 
-axios.defaults.headers.common["x-api-key"] = API_KEY;
+axios.defaults.headers.common['x-api-key'] = API_KEY;
 
 export function fetchBreeds() {
     return axios.get(`${BASE_URL}/${END_POINT_BREEDS}`);
--- src/index.js
+++ src/index.js
@@ -3,12 +3,11 @@ import { fetchBreeds } from './cat-api';
 import { fetchCatByBreed } from './cat-api';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SlimSelect from 'slim-select';
-//import SlimSelect from '@slim-select/vue';
 
 const refs = {
     select: document.querySelector('.js-breed-select'),
-    catsInfo: document.querySelector('js-cat-info'),
-    loader: document.querySelector('js-loader'),
+    catsInfo: document.querySelector('.js-cat-info'),
+    loader: document.querySelector('.js-loader'),
 };
 const IS_HIDDEN = 'is-hidden';
 
@@ -18,16 +17,16 @@ Notify.init({
     timeout: 3000,
     cssAnimationStyle: 'zoom',
     fontFamily: 'Arial, sans-serif',
-  });
+});
 
-  refs.select.classList.add(IS_HIDDEN);
-  refs.catsInfo.classList.add(IS_HIDDEN);
+refs.select.classList.add(IS_HIDDEN);
+refs.catsInfo.classList.add(IS_HIDDEN);
 
-  refs.select.addEventListener('change', selectChangeHandler);
+refs.select.addEventListener('change', selectChangeHandler);
 
-  showFetchedBreeds();
+showFetchedBreeds();
 
-  function selectChangeHandler(event) {
+function selectChangeHandler(event) {
     const selectedCatIndex = event.currentTarget.selectedIndex;
     const selectedId = event.currentTarget[selectedCatIndex].value;
 
@@ -36,43 +35,43 @@ Notify.init({
     refs.catsInfo.innerHTML = '';
 
     showFetchedCatBreed(selectedId);
-  }
-  function showFetchedBreeds() {
+}
+
+function showFetchedBreeds() {
     fetchBreeds()
-    .then(breeds => {  
-      refs.select.insertAdjacentHTML('beforeend',
-    createMarkup(breeds.data)
-    );
+        .then(breeds => {
+            refs.select.insertAdjacentHTML(
+                'beforeend',
+                createMarkup(breeds.data)
+            );
 
-    new SlimSelect({
-        select: '#single',
-        settings: {
-            placeholderText: 'Select the desired cat'
-        },
-    });
-    refs.loader.classList.add(IS_HIDDEN);
-    refs.select.classList.remove(IS_HIDDEN);
-  })
-  .catch(() => {
-    refs.loader.classList.add(IS_HIDDEN);
-    Notify.warning('Failed to reguest data! Choose another breed.');
+            new SlimSelect({
+                select: '#single',
+                settings: {
+                    placeholderText: 'Select the desired cat'
+                },
+            });
 
-  });
-  }
- 
+            refs.loader.classList.add(IS_HIDDEN);
+            refs.select.classList.remove(IS_HIDDEN);
+        })
+        .catch(() => {
+            refs.loader.classList.add(IS_HIDDEN);
+            Notify.warning('Failed to request data! Choose another breed.');
+        });
+}
 
 function showFetchedCatBreed(selectedId) {
     fetchCatByBreed(selectedId)
-    .then(cat => {
-        refs.loader.classList.add(IS_HIDDEN);
-        refs.catsInfo.classList.remove(IS_HIDDEN);
-        refs.catsInfo.innerHTML = createCatMarkup(cat.data[0]);
-    })
-.catch(() => {
-    refs.catsInfo.classList.add(IS_HIDDEN);
-    Notify.warning('Failed to request data! Choose another breed.');
-
-});
+        .then(cat => {
+            refs.loader.classList.add(IS_HIDDEN);
+            refs.catsInfo.classList.remove(IS_HIDDEN);
+            refs.catsInfo.innerHTML = createCatMarkup(cat.data[0]);
+        })
+        .catch(() => {
+            refs.catsInfo.classList.add(IS_HIDDEN);
+            Notify.warning('Failed to request data! Choose another breed.');
+        });
 }
 
 function createCatMarkup({ breeds, url }) {
@@ -84,13 +83,12 @@ function createCatMarkup({ breeds, url }) {
       <p class="cat-temperament"><span>Temperament:</span> ${temperament}</p>
   `;
 }
- 
- function createMarkup(breeds) {
-    return (`<option data-placeholder="true"></option>` + 
-    breeds.map(breed => createOptionMarkup(breed)).join(''));
 
- }
+function createMarkup(breeds) {
+    return (`<option data-placeholder="true"></option>` +
+        breeds.map(breed => createOptionMarkup(breed)).join(''));
+}
 
- function createOptionMarkup({ id, name }) {
+function createOptionMarkup({ id, name }) {
     return `<option value="${id}">${name}</option>`;
- }
+}
